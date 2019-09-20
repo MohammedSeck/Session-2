05e3b5aab5c5e5973d401628020dd50dd1cdec24
MohammedSeck <55282159+MohammedSeck@users.noreply.github.com>
2019-09-20 16:08:34 +0100

    Add

---
+++ Session2/Maths.js
@@ -0,0 +1,180 @@
+function q1()
+{
+  answer1= "Correct"
+  answer2= "Incorrect"
+
+  answerOne = prompt("What is 42-28?","")
+
+
+  if (answerOne == "14") {
+      document.getElementById("a1").innerHTML = answer1
+  }
+
+  else
+  if (answerOne != "14") {
+      document.getElementById("a1").innerHTML = answer2
+  }
+}
+
+function q2()
+{
+  answer3= "Correct"
+  answer4= "Incorrect"
+
+  answerTwo = prompt("What is 12*3?","")
+
+
+  if (answerTwo == "36") {
+      document.getElementById("a2").innerHTML = answer3
+  }
+
+  else
+  if (answerTwo != "36") {
+      document.getElementById("a2").innerHTML = answer4
+  }
+}
+
+function q3()
+{
+  answer5= "Correct"
+  answer6= "Incorrect"
+
+  answerThree = prompt("What is 23-3?","")
+
+
+  if (answerThree == "23") {
+      document.getElementById("a3").innerHTML = answer5
+  }
+
+  else
+  if (answerThree != "23") {
+      document.getElementById("a3").innerHTML = answer6
+  }
+}
+
+function q4()
+{
+  answer7= "Correct"
+  answer8= "Incorrect"
+
+  answerFour = prompt("What is 12/6?","")
+
+
+  if (answerFour == "2") {
+      document.getElementById("a4").innerHTML = answer7
+  }
+
+  else
+  if (answerFour != "2) {
+      document.getElementById("a4").innerHTML = answer8
+  }
+}
+
+function q5()
+{
+  answer9= "Correct"
+  answer10= "Incorrect"
+
+  answerFive = prompt("What is 34+23?","")
+
+
+  if (answerFive == "57") {
+      document.getElementById("a5").innerHTML = answer9
+  }
+
+  else
+  if (answerFive != "57") {
+      document.getElementById("a5").innerHTML = answer10
+  }
+}
+
+function q6()
+{
+  answer11= "Correct"
+  answer12= "Incorrect"
+
+  answerSix = prompt("What is 34-2?","")
+
+
+  if (answerSix == "32") {
+      document.getElementById("a6").innerHTML = answer11
+  }
+
+  else
+  if (answerSix != "32") {
+      document.getElementById("a6").innerHTML = answer12
+  }
+}
+
+
+function q7()
+{
+  answer13= "Correct"
+  answer14= "Incorrect"
+
+  answerSeven = prompt("What is 48/4?","")
+
+
+  if (answerSeven == "12") {
+      document.getElementById("a7").innerHTML = answer13
+  }
+
+  else
+  if (answerSeven != "12") {
+      document.getElementById("a7").innerHTML = answer14
+  }
+}
+
+function q8()
+{
+  answer15= "Correct"
+  answer16= "Incorrect"
+
+  answerEight = prompt("What is 7*3?","")
+
+
+  if (answerEight == "21") {
+      document.getElementById("a8").innerHTML = answer15
+  }
+
+  else
+  if (answerEight != "21") {
+      document.getElementById("a8").innerHTML = answer16
+  }
+}
+
+function q9()
+{
+  answer17= "Correct"
+  answer18= "Incorrect"
+
+  answerNine = prompt("What is 43*4?","")
+
+
+  if (answerNine == "172") {
+      document.getElementById("a9").innerHTML = answer17
+  }
+
+  else
+  if (answerNine != "172") {
+      document.getElementById("a9").innerHTML = answer18
+  }
+}
+
+function q10()
+{
+  answer19= "Correct"
+  answer20= "Incorrect"
+
+  answerTen = prompt("What is 81-25?","")
+
+
+  if (answerTen == "56") {
+      document.getElementById("a10").innerHTML = answer19
+  }
+
+  else
+  if (answerTen != "56") {
+      document.getElementById("a10").innerHTML = answer20
+  }
+}
